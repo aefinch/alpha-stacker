10ab721b7d00ffbdc31ba663e5a9eefc1ebad067
Andrea Finch <aefinch@vt.edu>
2017-02-09 22:37:41 -0600

    minor corrections

--- alpha-stacker.js
+++ alpha-stacker.js
@@ -1,5 +1,5 @@
 // Create an array that contains the letters of the alphabet
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // The stackLetter function should accept the array as the sole argument
 function stackLetters (theAlphabetArray) {
@@ -7,17 +7,15 @@ function stackLetters (theAlphabetArray) {
       Write a `for` loop that iterates over the array argument and
       outputs the letters.
      */
-     var currentRow=alphabet[0];
-     var rowNone=alphabet[0];
-     for (i=1; i<alphabet.length; i++) {
+     var currentRow=theAlphabetArray[0];
+     var rowNone=theAlphabetArray[0];
+     for (i=1; i<theAlphabetArray.length; i++) {
      	console.log(currentRow);
-     	// currentLength=currentRow.length;
-     	if (rowNone.
-     		   .length % 3 ===0) {
+     	if (rowNone.length % 3 ===0) {
      		currentRow+=" "
      	}
-     	currentRow+=alphabet[i];
-     	rowNone+=alphabet[i];
+     	currentRow+=theAlphabetArray[i];
+     	rowNone+=theAlphabetArray[i];
      }
 
 }
